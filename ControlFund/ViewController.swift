c3c309a92bf11beb0036b1387595ad852f7fe64c
Mak Chun Pong Danny <Danny@Maks-iMac.local>
2017-09-22 21:37:51 +0800

    Initial Commit

---
+++ ControlFund/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ControlFund
+//
+//  Created by Mak Chun Pong Danny on 22/9/2017.
+//  Copyright © 2017 DEVELOPAPA LIMITED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
